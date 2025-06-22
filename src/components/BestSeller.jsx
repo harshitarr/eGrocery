d00abaddad739aaaa11ff256400da684cc084b14
Harshita <harshitarera@gmail.com>
2025-06-22 23:21:43 +0530

    best-sellers

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -8,8 +8,10 @@ const BestSeller = () => {
   return (
     <div className='mt-16'>
         <p className='text-2xl font-semibold'>Best Sellers</p>
-        <div>
-           <ProductCard product={products[0]} />
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5'>
+           {products.filter((product)=>product.inStock).slice(13,18).map((product,index)=>(
+            <ProductCard key={index} product={product}/>
+           ))}
         </div>
     </div>
   )
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,28 +10,18 @@ const ProductCard = ({ product }) => {
     addToCart(product);
   };
 
-  const increment = () => {
-    addToCart(product);
-  };
 
-  const decrement = () => {
-    if (cartItems[product._id] > 1) {
-      removeFromCart(product._id, false); // reduce quantity by 1
-    } else {
-      removeFromCart(product._id, true); // remove product completely
-    }
-  };
 
   return product && (
-    <div className="bg-white border border-gray-300 rounded-lg shadow-sm hover:shadow-md transition-all w-full max-w-[220px] overflow-hidden">
+    <div className="bg-white border border-gray-300 rounded-lg shadow-sm hover:shadow-md transition-all mt-7 w-full max-w-[220px] overflow-hidden">
       <img
         src={product.image[0]}
         alt={product.name}
-        className="h-34 object-contain mx-auto"
+        className="h-34 object-contain mx-auto cursor-pointer hover:scale-110 transition"
       />
 
       {/* Bottom gray section */}
-      <div className="bg-gray-100 px-3 pt-2 pb-3 rounded-b-lg relative">
+      <div className="bg-gray-100 px-3 pt-2 pb-3 rounded-b-lg relative cursor-pointer">
         <p className="text-xs text-gray-500/60">{product.category}</p>
         <p className="text-sm font-semibold text-gray-800 truncate">{product.name}</p>
 
@@ -54,16 +44,16 @@ const ProductCard = ({ product }) => {
         <div onClick = {(e) =>{ e.stopPropagation()}}className="absolute bottom-2 right-3">
           {!cartItems[product._id] ? (
             <button
-              onClick={handleAddToCart}
+              onClick={() => addToCart(product._id)}
               className="w-7 h-7 flex items-center text-white justify-center rounded-full bg-green-600 hover:bg-green-400 cursor-pointer transition text-sm"
             >
               <FiShoppingCart size={15} />
             </button>
           ) : (
             <div className="flex items-center bg-green-600 rounded-full px-2 py-0.5 space-x-2 text-white text-xs">
-              <button onClick={decrement} className="px-1 cursor-pointer">-</button>
+              <button onClick={() => {removeFromCart(product._id)}} className="px-1 cursor-pointer">-</button>
               <span>{cartItems[product._id]}</span>
-              <button onClick={increment} className="px-1 cursor-pointer">+</button>
+              <button onClick={()=>{addToCart(product._id)}} className="px-1 cursor-pointer">+</button>
             </div>
           )}
         </div>
