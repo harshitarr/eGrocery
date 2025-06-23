d40feec324792440fb9a1f0dae83bac108f64c70
Harshita <harshitarera@gmail.com>
2025-06-23 13:44:33 +0530

    bottom

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -9,7 +9,7 @@ const BottomBanner = () => {
       <img src={bottom} alt='bottom_banner' className='w-full hidden md:block' />
       <img src={bottoms} alt='bottom_banner' className='w-full  md:hidden' />
 
-      <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
+      <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24 lg:mr-9'>
           <div>
               <h1 className='text-2xl md:text-2xl lg:text-3xl font-bold text-primary mb-6 md:mb-3 lg:mb-6'>
                   Why We Are The Best?
