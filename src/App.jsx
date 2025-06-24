c2a110d7fa8092b8ddeaae722fd53de90cc7af3d
Harshita <harshitarera@gmail.com>
2025-06-24 22:39:44 +0530

    login-page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { Route,Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
+import Login from './components/Login'
+import { useAppContext } from './context/AppContext'; // Important!
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
+  const {showUserLogin} = useAppContext()
   return (
     <>
       {isSellerPath? null : <Navbar/>}          {/* If we are in the seller login it removes the navigation bar*/}
+      {showUserLogin ? <Login/>:null}
       <Toaster/>
       <div className={`${isSellerPath ?"": "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAppContext } from '../context/AppContext'
+
+const AllProducts = () => {
+
+const {products} = useAppContext()
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AllProducts
